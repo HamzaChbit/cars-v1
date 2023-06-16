8b733899f227e1dce18bbbad5f20aadc69b3d460
HamzaChbit <hamzachbit@gmail.com>
2023-06-16 12:32:45 +0100

    first commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,26 @@
+'use client'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import CustomButton from './CustomButton'
+
+const Navbar = () => {
+  return (
+    <header  className='w-full absolute z-10  m-0 p-0' >
+        <nav className='max-w-[1440px]   mx-auto flex justify-between items-center sm:px-16 px-6 py-6' >
+            <Link   className='flex justify-center items-center'  href='/'      >
+            <Image  src='/logo.svg'   alt='Car Logo' width={180}  height={20} className='object-contain   m-0 p-0  '  />
+            </Link>
+        <CustomButton
+                  title='Sign In '
+                  btnType='button'
+                  containerStyles='text-primary-blue rounded-full bg-white min-w-[130px]' handleClick={function (event: React.MouseEvent<HTMLButtonElement, MouseEvent>): void {
+                      throw new Error('Function not implemented.')
+                  } }/>
+
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
